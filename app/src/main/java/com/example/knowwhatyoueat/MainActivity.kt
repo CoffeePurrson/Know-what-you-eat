dfbcd7141a20d3d352e260b2a322b6484b69fcf6
JosephineStoehr <JosephineStoehr@outlook.de>
2022-02-12 10:59:02 +0100

    Added: Permission INTERNET, Test-API-Call :)

--- app/src/main/java/com/example/knowwhatyoueat/MainActivity.kt
+++ app/src/main/java/com/example/knowwhatyoueat/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.knowwhatyoueat
 
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.knowwhatyoueat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +23,26 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val textView = binding.tvTest
+
+
+        binding.buTest.setOnClickListener {
+            // Instantiate the RequestQueue.
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://world.openfoodfacts.org/api/v0/product/737628064502"
+
+            // Request a string response from the provided URL.
+            val stringRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                Response.Listener { response ->
+                    // Display the first 500 characters of the response string.
+                    textView.text = "${response.toString().substring(0, 500)}"
+                },
+                Response.ErrorListener { textView.text = "That didn't work!" })
+
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest)
+        }
+
 
         val navView: BottomNavigationView = binding.navView
 
