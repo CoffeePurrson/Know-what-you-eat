ba59e586e05e486b00af3b45e9361ce0a5003998
Vanessa Nock <vanessa.nock@bwb.de>
2022-02-22 12:09:11 +0100

    updated JSON-parsing

--- app/src/main/java/com/example/knowwhatyoueat/ProductView.kt
+++ app/src/main/java/com/example/knowwhatyoueat/ProductView.kt
@@ -1,7 +1,9 @@
 package com.example.knowwhatyoueat
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.knowwhatyoueat.databinding.ActivityProductViewBinding
 import org.json.JSONObject
 
@@ -9,6 +11,7 @@ class ProductView : AppCompatActivity() {
 
     private lateinit var binding: ActivityProductViewBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProductViewBinding.inflate(layoutInflater)
@@ -24,12 +27,53 @@ class ProductView : AppCompatActivity() {
         val foodJSON = jsonObj.getJSONObject("product")
 
         for (i in 0 until foodJSON.length()) {
-            var  name = foodJSON.getString(("product_name"))
-            binding.textView18.text=name.toString()
-            var hersteller=foodJSON.getString("brand_owner")
-            binding.textView20.text =hersteller.toString()
-            var zutaten=foodJSON.getString("ingredients_text")
-            binding.textView31.text= zutaten.toString()
+            val  name = foodJSON.getString(("product_name"))
+            binding.tvProduktname.text=name.toString()
+            val hersteller=foodJSON.getString("brand_owner")
+            binding.tvHerstellername.text =hersteller.toString()
+            val zutaten=foodJSON.getString("ingredients_text")
+            binding.tvInhaltsstoffe.text= zutaten.toString()
+            val herstellungsland = foodJSON.getString("origins")
+            binding.tvOrigin.text = herstellungsland.toString()
+
+            val portion = foodJSON.getString("serving_quantity")
+            binding.tvPortion.text = ("Pro Portion (${portion.toString()}g)")
+
+            val naehrwerte = foodJSON.getJSONObject("nutriments")
+            val energie100 = naehrwerte.getString("energy-kcal_100g")
+            val energieEinheit = naehrwerte.getString("energy-kcal_unit")
+            binding.tvEnergie100.text = energie100.toString() + energieEinheit.toString()
+            val kohlenhydrat100 = naehrwerte.getString("carbohydrates_100g")
+            val kohlenhydratPortion = naehrwerte.getString("carbohydrates_serving")
+            val kohlenhydratEinheit = naehrwerte.getString("carbohydrates_unit")
+            binding.tvKohlenhydrat100.text = kohlenhydrat100.toString() + kohlenhydratEinheit.toString()
+            binding.tvKohlenhydratPortion.text = kohlenhydratPortion.toString() + kohlenhydratEinheit.toString()
+            val fett100 = naehrwerte.getString("fat_100g")
+            val fettPortion = naehrwerte.getString("fat_serving")
+            val fettEinheit = naehrwerte.getString("fat_unit")
+            binding.tvFett100.text = fett100.toString() + fettEinheit.toString()
+            binding.tvFettPortion.text = fettPortion.toString() + fettEinheit.toString()
+            val fettsaeure100 = naehrwerte.getString("saturated-fat_100g")
+            val fettsaeurePortion = naehrwerte.getString("saturated-fat_serving")
+            val fettsaeureEinheit = naehrwerte.getString("saturated-fat_unit")
+            binding.tvFettsaeuren100.text = fettsaeure100.toString() + fettsaeureEinheit.toString()
+            binding.tvFettsaeurenPortion.text = fettsaeurePortion.toString() + fettsaeureEinheit.toString()
+            val zucker100 = naehrwerte.getString("sugars_100g")
+            val zuckerPortion = naehrwerte.getString("sugars_serving")
+            val zuckerEinheit = naehrwerte.getString("sugars_unit")
+            binding.tvZucker100.text = zucker100.toString() + zuckerEinheit.toString()
+            binding.tvZuckerPortion.text = zuckerPortion.toString() + zuckerEinheit.toString()
+            val protein100 = naehrwerte.getString("proteins_100g")
+            val proteinPortion = naehrwerte.getString("proteins_serving")
+            val proteinEinheit = naehrwerte.getString("proteins_unit")
+            binding.tvProtein100.text = protein100.toString() + proteinEinheit.toString()
+            binding.tvProteinPortion.text = proteinPortion.toString() + proteinEinheit.toString()
+            val salz100 = naehrwerte.getString("salt_100g")
+            val salzPortion = naehrwerte.getString("salt_serving")
+            val salzEinheit = naehrwerte.getString("salt_unit")
+            binding.tvSalz100.text = salz100.toString() + salzEinheit.toString()
+            binding.tvSalzPortion.text = salzPortion.toString() + salzEinheit.toString()
+
         }
 
         binding.Zurueck.setOnClickListener{ finish()  }
