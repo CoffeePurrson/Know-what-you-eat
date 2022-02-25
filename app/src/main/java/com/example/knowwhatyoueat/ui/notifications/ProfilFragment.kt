1cdf0effd44abfb3650d17fc79df61be2e8495b0
JosephineStoehr <JosephineStoehr@outlook.de>
2022-02-25 09:54:38 +0100

    NotificationsFragment refactored

--- app/src/main/java/com/example/knowwhatyoueat/ui/notifications/ProfilFragment.kt
+++ app/src/main/java/com/example/knowwhatyoueat/ui/notifications/ProfilFragment.kt
@@ -4,17 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.knowwhatyoueat.R
-import com.example.knowwhatyoueat.databinding.FragmentNotificationsBinding
+import com.example.knowwhatyoueat.databinding.FragmentProfilBinding
 
-class NotificationsFragment : Fragment() {
+class ProfilFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var profilViewModel: ProfilViewModel
+    private var _binding: FragmentProfilBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        profilViewModel =
+            ViewModelProvider(this).get(ProfilViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentProfilBinding.inflate(inflater, container, false)
         val root: View = binding.root
 /*
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        profilViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
 
