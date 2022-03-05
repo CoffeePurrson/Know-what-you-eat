1cafce05026bca843db16ab25c22434accff1913
Vanessa Nock <vanessa.nock@bwb.de>
2022-03-05 13:34:36 +0100

    updatet json-parsing

--- app/src/main/java/com/example/knowwhatyoueat/ProductView.kt
+++ app/src/main/java/com/example/knowwhatyoueat/ProductView.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import com.example.knowwhatyoueat.databinding.ActivityProductViewBinding
+import org.json.JSONException
 import org.json.JSONObject
 
 
@@ -24,87 +25,186 @@ class ProductView : AppCompatActivity() {
             else
                 "Das hat leider nicht funktioniert."
 
-        val jsonObj = JSONObject(z?.substring(z?.indexOf("{"), z?.lastIndexOf("}")+1))
+        val jsonObj = JSONObject(z?.substring(z?.indexOf("{"), z?.lastIndexOf("}") + 1))
         val foodJSON = jsonObj.getJSONObject("product")
 
         for (i in 0 until foodJSON.length()) {
-            val  name = foodJSON.getString(("product_name"))
-            binding.tvProduktname.text=name.toString()
-            val hersteller=foodJSON.getString("brand_owner")
-            binding.tvHerstellername.text =hersteller.toString()
-            val zutaten=foodJSON.getString("ingredients_text")
-            binding.tvInhaltsstoffe.text= zutaten.toString()
-            val herstellungsland = foodJSON.getString("origins")
-            binding.tvOrigin.text = herstellungsland.toString()
+            if(foodJSON.has("product_name")){
+                val name = foodJSON.getString(("product_name"))
+                binding.tvProduktname.text = name.toString()
+            }else
+                binding.tvProduktname.text = "unknown"
 
-            val portion = foodJSON.getString("serving_quantity")
-            binding.tvPortion.text = ("Pro Portion (${portion.toString()}g)")
+            if(foodJSON.has("brands")){
+                val hersteller = foodJSON.getString("brands")
+                binding.tvHerstellername.text = hersteller.toString()
+            }else
+                binding.tvHerstellername.text = "unknown"
 
-            val naehrwerte = foodJSON.getJSONObject("nutriments")
-            val energie100 = naehrwerte.getString("energy-kcal_100g")
-            val energiePortion = naehrwerte.getString("energy-kcal_serving")
-            val energieEinheit = naehrwerte.getString("energy-kcal_unit")
-            binding.tvEnergie100.text = energie100.toString() + energieEinheit.toString()
-            binding.tvEnergiePortion.text = energiePortion.toString() + energieEinheit.toString()
-            val kohlenhydrat100 = naehrwerte.getString("carbohydrates_100g")
-            val kohlenhydratPortion = naehrwerte.getString("carbohydrates_serving")
-            val kohlenhydratEinheit = naehrwerte.getString("carbohydrates_unit")
-            binding.tvKohlenhydrat100.text = kohlenhydrat100.toString() + kohlenhydratEinheit.toString()
-            binding.tvKohlenhydratPortion.text = kohlenhydratPortion.toString() + kohlenhydratEinheit.toString()
-            val fett100 = naehrwerte.getString("fat_100g")
-            val fettPortion = naehrwerte.getString("fat_serving")
-            val fettEinheit = naehrwerte.getString("fat_unit")
-            binding.tvFett100.text = fett100.toString() + fettEinheit.toString()
-            binding.tvFettPortion.text = fettPortion.toString() + fettEinheit.toString()
-            val fettsaeure100 = naehrwerte.getString("saturated-fat_100g")
-            val fettsaeurePortion = naehrwerte.getString("saturated-fat_serving")
-            val fettsaeureEinheit = naehrwerte.getString("saturated-fat_unit")
-            binding.tvFettsaeuren100.text = fettsaeure100.toString() + fettsaeureEinheit.toString()
-            binding.tvFettsaeurenPortion.text = fettsaeurePortion.toString() + fettsaeureEinheit.toString()
-            val zucker100 = naehrwerte.getString("sugars_100g")
-            val zuckerPortion = naehrwerte.getString("sugars_serving")
-            val zuckerEinheit = naehrwerte.getString("sugars_unit")
-            binding.tvZucker100.text = zucker100.toString() + zuckerEinheit.toString()
-            binding.tvZuckerPortion.text = zuckerPortion.toString() + zuckerEinheit.toString()
-            val protein100 = naehrwerte.getString("proteins_100g")
-            val proteinPortion = naehrwerte.getString("proteins_serving")
-            val proteinEinheit = naehrwerte.getString("proteins_unit")
-            binding.tvProtein100.text = protein100.toString() + proteinEinheit.toString()
-            binding.tvProteinPortion.text = proteinPortion.toString() + proteinEinheit.toString()
-            val salz100 = naehrwerte.getString("salt_100g")
-            val salzPortion = naehrwerte.getString("salt_serving")
-            val salzEinheit = naehrwerte.getString("salt_unit")
-            binding.tvSalz100.text = salz100.toString() + salzEinheit.toString()
-            binding.tvSalzPortion.text = salzPortion.toString() + salzEinheit.toString()
+            if(foodJSON.has("ingredients_text")){
+                val zutaten = foodJSON.getString("ingredients_text")
+                binding.tvInhaltsstoffe.text = zutaten.toString()
+            }else
+                binding.tvInhaltsstoffe.text = "unknown"
 
-            val produktImage = foodJSON.getString("image_front_small_url")
-            com.squareup.picasso.Picasso.get().load(produktImage).into(binding.ivProduktImage)
+            if(foodJSON.has("origins")){
+                val herstellungsland = foodJSON.getString("origins")
+                binding.tvOrigin.text = herstellungsland.toString()
+            }else
+                binding.tvOrigin.text = "unknown"
 
-            val novaScore = foodJSON.getString("nova_group")
-            if(novaScore == "1")
-                binding.ivNovascore.setImageResource(R.drawable.nova_group_1)
-            else if(novaScore == "2")
-                binding.ivNovascore.setImageResource(R.drawable.nova_group_2)
-            else if(novaScore == "3")
-                binding.ivNovascore.setImageResource(R.drawable.nova_group_3)
-            else if(novaScore=="4")
-                binding.ivNovascore.setImageResource(R.drawable.nova_group_4)
-            else
-                binding.ivNovascore.setImageResource(R.drawable.nova_group_unknown)
 
-            val nutriScore = foodJSON.getString("nutriscore_grade")
-            if(nutriScore == "a")
-                binding.ivNutriScore.setImageResource(R.drawable.nutriscore_a)
-            else if(nutriScore == "b")
-                binding.ivNutriScore.setImageResource(R.drawable.nutriscore_b)
-            else if(nutriScore == "c")
-                binding.ivNutriScore.setImageResource(R.drawable.nutriscore_c)
-            else if(nutriScore == "d")
-                binding.ivNutriScore.setImageResource(R.drawable.nutriscore_d)
-            else if(nutriScore == "e")
-                binding.ivNutriScore.setImageResource(R.drawable.nutriscore_e)
-            else
-                binding.ivNutriScore.setImageResource(R.drawable.nutriscore_unknown)
+            if(foodJSON.has("serving_quantity")) {
+                val portion = foodJSON.getString("serving_quantity")
+                binding.tvPortion.text = ("Pro Portion (${portion.toString()}g)")
+            }
+
+            try {
+                val naehrwerte = foodJSON.getJSONObject("nutriments")
+                if(naehrwerte.has("energy-kcal_100g")&&naehrwerte.has("energy-kcal_unit")) {
+                    val energie100 = naehrwerte.getString("energy-kcal_100g")
+                    val energieEinheit = naehrwerte.getString("energy-kcal_unit")
+                    binding.tvEnergie100.text = energie100.toString() + energieEinheit.toString()
+                }else
+                    binding.tvEnergie100.text = "unknown"
+                if(naehrwerte.has("energy-kcal_serving")&&naehrwerte.has("energy-kcal_unit")) {
+                    val energiePortion = naehrwerte.getString("energy-kcal_serving")
+                    val energieEinheit = naehrwerte.getString("energy-kcal_unit")
+                    binding.tvEnergiePortion.text = energiePortion.toString() + energieEinheit.toString()
+                }else
+                    binding.tvEnergiePortion.text = "unknown"
+
+                if(naehrwerte.has("carbohydrates_100g")&&naehrwerte.has("energy-kcal_unit")) {
+                    val kohlenhydrat100 = naehrwerte.getString("carbohydrates_100g")
+                    val kohlenhydratEinheit = naehrwerte.getString("carbohydrates_unit")
+                    binding.tvKohlenhydrat100.text = kohlenhydrat100.toString() + kohlenhydratEinheit.toString()
+                } else
+                    binding.tvKohlenhydrat100.text = "unknown"
+                if(naehrwerte.has("carbohydrates_serving")&&naehrwerte.has("carbohydrates_unit")) {
+                    val kohlenhydratPortion = naehrwerte.getString("carbohydrates_serving")
+                    val kohlenhydratEinheit = naehrwerte.getString("carbohydrates_unit")
+                    binding.tvKohlenhydratPortion.text = kohlenhydratPortion.toString() + kohlenhydratEinheit.toString()
+                } else
+                    binding.tvKohlenhydratPortion.text = "unknown"
+
+                if(naehrwerte.has("fat_100g")&& naehrwerte.has("fat_unit")){
+                    val fett100 = naehrwerte.getString("fat_100g")
+                    val fettEinheit = naehrwerte.getString("fat_unit")
+                    binding.tvFett100.text = fett100.toString() + fettEinheit.toString()
+                } else
+                    binding.tvFett100.text = "unknown"
+                if(naehrwerte.has("fat_serving")&& naehrwerte.has("fat_unit")){
+                    val fettPortion = naehrwerte.getString("fat_serving")
+                    val fettEinheit = naehrwerte.getString("fat_unit")
+                    binding.tvFettPortion.text = fettPortion.toString() + fettEinheit.toString()
+                } else
+                    binding.tvFettPortion.text = "unknown"
+
+                if(naehrwerte.has("saturated-fat_100g") && naehrwerte.has("saturated-fat_unit")){
+                    val fettsaeure100 = naehrwerte.getString("saturated-fat_100g")
+                    val fettsaeureEinheit = naehrwerte.getString("saturated-fat_unit")
+                    binding.tvFettsaeuren100.text = fettsaeure100.toString() + fettsaeureEinheit.toString()
+                } else
+                    binding.tvFettsaeuren100.text = "unknown"
+                if(naehrwerte.has("saturated-fat_serving") && naehrwerte.has("saturated-fat_unit")){
+                    val fettsaeurePortion = naehrwerte.getString("saturated-fat_serving")
+                    val fettsaeureEinheit = naehrwerte.getString("saturated-fat_unit")
+                    binding.tvFettsaeurenPortion.text = fettsaeurePortion.toString() + fettsaeureEinheit.toString()
+                } else
+                    binding.tvFettsaeurenPortion.text = "unknown"
+
+                if(naehrwerte.has("sugars_100g") && naehrwerte.has("sugars_unit")){
+                    val zucker100 = naehrwerte.getString("sugars_100g")
+                    val zuckerEinheit = naehrwerte.getString("sugars_unit")
+                    binding.tvZucker100.text = zucker100.toString() + zuckerEinheit.toString()
+                } else
+                    binding.tvZucker100.text = "unknown"
+                if(naehrwerte.has("sugars_serving") && naehrwerte.has("sugars_unit")){
+                    val zuckerPortion = naehrwerte.getString("sugars_serving")
+                    val zuckerEinheit = naehrwerte.getString("sugars_unit")
+                    binding.tvZuckerPortion.text = zuckerPortion.toString() + zuckerEinheit.toString()
+                } else
+                    binding.tvZuckerPortion.text = "unknown"
+
+                if(naehrwerte.has("proteins_100g") && naehrwerte.has("proteins_unit")){
+                    val protein100 = naehrwerte.getString("proteins_100g")
+                    val proteinEinheit = naehrwerte.getString("proteins_unit")
+                    binding.tvProtein100.text = protein100.toString() + proteinEinheit.toString()
+                } else
+                    binding.tvProtein100.text = "unknown"
+                if(naehrwerte.has("proteins_serving") && naehrwerte.has("proteins_unit")){
+                    val proteinPortion = naehrwerte.getString("proteins_serving")
+                    val proteinEinheit = naehrwerte.getString("proteins_unit")
+                    binding.tvProteinPortion.text = proteinPortion.toString() + proteinEinheit.toString()
+                } else
+                    binding.tvProteinPortion.text = "unknown"
+
+                if(naehrwerte.has("salt_100g") && naehrwerte.has("salt_unit")){
+                    val salz100 = naehrwerte.getString("salt_100g")
+                    val salzEinheit = naehrwerte.getString("salt_unit")
+                    binding.tvSalz100.text = salz100.toString() + salzEinheit.toString()
+                } else
+                    binding.tvSalz100.text = "unknown"
+                if(naehrwerte.has("salt_serving") && naehrwerte.has("salt_unit")){
+                    val salzPortion = naehrwerte.getString("salt_serving")
+                    val salzEinheit = naehrwerte.getString("salt_unit")
+                    binding.tvSalzPortion.text = salzPortion.toString() + salzEinheit.toString()
+                } else
+                    binding.tvSalzPortion.text = "unknown"
+            }catch (e:Exception){
+
+            }
+
+            if(foodJSON.has("image_front_small_url")) {
+                val produktImage = foodJSON.getString("image_front_small_url")
+                com.squareup.picasso.Picasso.get().load(produktImage).into(binding.ivProduktImage)
+            }
+
+            if(foodJSON.has("nova_group")) {
+                val novaScore = foodJSON.getString("nova_group")
+                if (novaScore == "1")
+                    binding.ivNovascore.setImageResource(R.drawable.nova_group_1)
+                else if (novaScore == "2")
+                    binding.ivNovascore.setImageResource(R.drawable.nova_group_2)
+                else if (novaScore == "3")
+                    binding.ivNovascore.setImageResource(R.drawable.nova_group_3)
+                else if (novaScore == "4")
+                    binding.ivNovascore.setImageResource(R.drawable.nova_group_4)
+            }else
+                    binding.ivNovascore.setImageResource(R.drawable.nova_group_unknown)
+
+            if(foodJSON.has("nutriscore_grade")) {
+                val nutriScore = foodJSON.getString("nutriscore_grade")
+                if (nutriScore == "a")
+                    binding.ivNutriScore.setImageResource(R.drawable.nutriscore_a)
+                else if (nutriScore == "b")
+                    binding.ivNutriScore.setImageResource(R.drawable.nutriscore_b)
+                else if (nutriScore == "c")
+                    binding.ivNutriScore.setImageResource(R.drawable.nutriscore_c)
+                else if (nutriScore == "d")
+                    binding.ivNutriScore.setImageResource(R.drawable.nutriscore_d)
+                else if (nutriScore == "e")
+                    binding.ivNutriScore.setImageResource(R.drawable.nutriscore_e)
+            }else
+                    binding.ivNutriScore.setImageResource(R.drawable.nutriscore_unknown)
+
+            if(foodJSON.has("ecoscore_grade")) {
+                val ecoScore = foodJSON.getString("ecoscore_grade")
+                if (ecoScore == "a")
+                    binding.ivEcoscore.setImageResource(R.drawable.ecoscore_a)
+                else if (ecoScore == "b")
+                    binding.ivEcoscore.setImageResource(R.drawable.ecoscore_b)
+                else if (ecoScore == "c")
+                    binding.ivEcoscore.setImageResource(R.drawable.ecoscore_c)
+                else if (ecoScore == "d")
+                    binding.ivEcoscore.setImageResource(R.drawable.ecoscore_d)
+                else if (ecoScore == "e")
+                    binding.ivEcoscore.setImageResource(R.drawable.ecoscore_e)
+                else
+                    binding.ivEcoscore.setImageResource(R.drawable.ecoscore_unknown)
+            }else
+                binding.ivEcoscore.setImageResource(R.drawable.ecoscore_unknown)
+
         }
 
         binding.Zurueck.setOnClickListener{ finish()  }
